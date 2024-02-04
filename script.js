e3c42a3d459d9260097eafa347c30ead7325fdcc
ARna06 <72038543+Leopard005537@users.noreply.github.com>
2024-02-04 18:16:21 +0530

    Add full functionality

--- script.js
+++ script.js
@@ -5,11 +5,9 @@ function getRandomNumber(num) {
 function choice(Num) {
     if (Num == 0) {
         return "Rock";
-    }
-    else if (Num == 1) {
+    } else if (Num == 1) {
         return "Paper";
-    }
-    else {
+    } else {
         return "Scissor";
     }
 }
@@ -27,24 +25,24 @@ function playground(computerSelection, playerSelection) {
         return "draw";
     }
     
-    if (c == "rock") {
-        if (p == "paper"){
+    if (p == "rock") {
+        if (c == "paper"){
             return "win";
         }
         else{
             return "lose";
         }
     }
-    if (c == "paper"){
-        if (p == "scissor"){
+    if (p == "paper"){
+        if (c == "scissor"){
             return "win";
         }
         else{
             return "lose";
         }
     }
-    if (c == "scissor"){
-        if (p == "rock"){
+    if (p == "scissor"){
+        if (c == "rock"){
             return "win";
         }
         else{
@@ -55,3 +53,48 @@ function playground(computerSelection, playerSelection) {
     return false;
 }
 
+function winCount (result, count) {
+    if ( result ==  'win' ){
+        return (count+1) ;
+    }
+    else {
+        return count;
+    }
+}
+
+function loseCount(result, num) {
+    if (result == 'lose'){
+        return (num + 1) ;
+    }
+    else{
+        return num;
+    }
+}
+
+alert("This is a simple rock paper scissor game against the computer."+ '\n'
+    + "Game ends when the player get 5 wins");
+
+count = 0
+totalMatch = 0;
+computerWin = 0;
+
+while (count <= 5){
+    let playerSelection = prompt('Make your move ');
+    let computerSelection = getComputerChoice();
+    let result = playground(computerSelection, playerSelection);
+
+    if (result == 'win' || result == 'lose') {
+        alert(`You ${result}`);
+    } 
+    if (result == 'draw'){
+        alert(`Computer uses ${computerSelection} and you use ${playerSelection}. \n The game is a draw`);
+    } 
+    if (result == false) {
+        alert('What an odd choice !');
+    }
+    count = winCount(result, count);
+    totalMatch += 1;
+    computerWin = loseCount(result, computerWin);
+}
+
+alert(`Out of ${totalMatch} games, you win in 5 rounds and computer wins in ${computerWin} rounds.`)
